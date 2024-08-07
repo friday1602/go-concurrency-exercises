0a8cd9e5943f58841176d38db0653437693e56c8
Friday1602 <krit.sriwong@gmail.com>
2024-08-07 16:39:56 +0700

    fin web-crawler ex

---
+++ 01-exercise/07-exercise-web-crawler/go.mod
@@ -0,0 +1,5 @@
+module github.com/friday1602/ex-web-crawler
+
+go 1.22.1
+
+require golang.org/x/net v0.28.0
--- 01-exercise/07-exercise-web-crawler/main.go
+++ 01-exercise/07-exercise-web-crawler/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"runtime"
 	"time"
 
 	"golang.org/x/net/html"
@@ -10,33 +11,57 @@ import (
 
 var fetched map[string]bool
 
+type result struct {
+	url   string
+	depth int
+	err   error
+	urls  []string
+}
+
 // Crawl uses findLinks to recursively crawl
 // pages starting with url, to a maximum of depth.
 func Crawl(url string, depth int) {
 	// TODO: Fetch URLs in parallel.
+	ch := make(chan *result)
 
-	if depth < 0 {
-		return
+	fetch := func(url string, depth int) {
+		urls, err := findLinks(url)
+		res := result{
+			url:   url,
+			depth: depth,
+			err:   err,
+			urls:  urls,
+		}
+		ch <- &res
 	}
-	urls, err := findLinks(url)
-	if err != nil {
-		// fmt.Println(err)
-		return
-	}
-	fmt.Printf("found: %s\n", url)
+
+	go fetch(url, depth)
 	fetched[url] = true
-	for _, u := range urls {
-		if !fetched[u] {
-			Crawl(u, depth-1)
+
+	for fetching := 1; fetching > 0; fetching-- {
+		res := <-ch
+		if res.err != nil {
+			continue
+		}
+		if res.depth < 0 {
+			break
+		}
+		for _, u := range res.urls {
+			fmt.Printf("found: %s\n", u)
+			if !fetched[u] {
+				fetching++
+				go fetch(u, res.depth-1)
+				fetched[u] = true
+			}
 		}
 	}
-	return
 }
 
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
 	fetched = make(map[string]bool)
 	now := time.Now()
-	Crawl("http://andcloud.io", 2)
+	Crawl("http://medium.com/", 2)
 	fmt.Println("time taken:", time.Since(now))
 }
 
